1effffce4dfcbb7f4b544c94dba74fdee558f6cc
kintanputia <kintanputia090101@gmail.com>
2021-10-04 09:26:01 +0700

    login logout register

---
+++ app/Models/Peserta.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Peserta extends Model
+{
+    use HasFactory;
+    protected $table='peserta';
+    protected $fillable = ['nim', 'nama', 'id_users', 'tgl_lahir'];
+    public $timestamps = false; 
+}
